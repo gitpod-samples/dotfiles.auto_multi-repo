5965d43bba2b6e0fc214c7897e9f430f90d41fed
Aminur Rahman <axonasif@gmail.com>
2025-05-06 14:13:37 +0000

    fix: handle multiple code-workspace files properly

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-
+set -eux
 (
 	if grep -q 'supervisor' /proc/1/cmdline; then {
-		gp ports await 23000 1>/dev/null;
-		code -r $GITPOD_REPO_ROOT/${DOTFILES_CODE_WORKSPACE:-*.code-workspace}
+		gp ports await 23000 1>/dev/null
+		cd "${GITPOD_REPO_ROOT}"
+		code_wss=(*.code-workspace)
+		code -r "${DOTFILES_CODE_WORKSPACE:-${code_wss[-1]}}"
 	} fi
 ) & disown
