a6adc6e41c49cee3a93c152c4c5bce0b750cd73e
Aminur Rahman <axonasif@gmail.com>
2025-05-06 13:59:16 +0000

    fix wildcard

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 (
 	if grep -q 'supervisor' /proc/1/cmdline; then {
 		gp ports await 23000 1>/dev/null;
-		code -r $GITPOD_REPO_ROOT/${DOTFILES_CODE_WORKSPACE:-'*.code-workspace'}
+		code -r $GITPOD_REPO_ROOT/${DOTFILES_CODE_WORKSPACE:-*.code-workspace}
 	} fi
 ) & disown
