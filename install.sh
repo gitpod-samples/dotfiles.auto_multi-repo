894c1365bf2d4ef924ea0280e4356bd000538a56
Aminur Rahman <axonasif@gmail.com>
2025-05-06 20:21:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-( 	if grep -q 'supervisor' /proc/1/cmdline; then {
+(
+	if grep -q 'supervisor' /proc/1/cmdline; then {
 		gp ports await 23000 1>/dev/null;
-    code -r $GITPOD_REPO_ROOT/*.code-workspace
+		code -r $GITPOD_REPO_ROOT/${DOTFILES_CODE_WORKSPACE:-'*.code-workspace'}
 	} fi
 ) & disown
